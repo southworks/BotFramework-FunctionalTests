9193a885ba840645180b9971d400973245aa46b7
LocalizationBuildProcess <v-bruhal@microsoft.com>
2020-11-24 15:04:26 -0800

    Try WarmupShouldSignIn()

--- Tests/SkillFunctionalTests/BotWarmupFixture.cs
+++ Tests/SkillFunctionalTests/BotWarmupFixture.cs
@@ -2,8 +2,11 @@
 // Licensed under the MIT License.
 
 using System;
+using System.IO;
+using System.Linq;
 using System.Threading;
 using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
 using TranscriptTestRunner;
 using TranscriptTestRunner.XUnit;
 using Xunit;
@@ -16,9 +19,11 @@ namespace SkillFunctionalTests
     /// </summary>
     public class BotWarmupFixture
     {
+        private readonly string _transcriptsFolder = Directory.GetCurrentDirectory() + @"/SourceTranscripts";
+        
         public BotWarmupFixture()
         {
-            var result = WarmupManualTest();
+            var result = WarmupShouldSignIn();
             result.Wait();
         }
 
@@ -42,6 +47,64 @@ namespace SkillFunctionalTests
                         Assert.Equal(ActivityTypes.Message, activity.Type);
                         Assert.Equal("Hello and welcome!", activity.Text);
                     });
+                }
+                catch (Exception e)
+                {
+                    if (retries > 0)
+                    {
+                        retries--;
+
+                        Console.WriteLine(e.Message);
+                        Console.WriteLine($"Waiting for warmup. Retries = {retries}");
+                        Thread.Sleep(timeBetweenRetriesMs);
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bot warmup failed.");
+                        throw new Exception($"Bot warmup failed: {e}");
+                    }
+                }
+
+                Console.WriteLine("Bot warmup completed.");
+                break;
+            }
+        }
+
+        private async System.Threading.Tasks.Task WarmupShouldSignIn()
+        {
+            Console.WriteLine("Starting bot warmup.");
+
+            var runner = new XUnitTestRunner(new TestClientFactory(ClientType.DirectLine).GetTestClient(), null);
+            var signInUrl = string.Empty;
+
+            int retries = 6;                        // Defines the allowed warmup period.
+            int timeBetweenRetriesMs = 30 * 1000;
+
+            while (retries >= 0)
+            {
+                try
+                {
+                    // Execute the first part of the conversation.
+                    await runner.RunTestAsync(Path.Combine(_transcriptsFolder, "ShouldSignIn1.transcript"));
+
+                    // Obtain the signIn url.
+                    await runner.AssertReplyAsync(activity =>
+                    {
+                        Assert.Equal(ActivityTypes.Message, activity.Type);
+                        Assert.True(activity.Attachments.Count > 0);
+
+                        var card = JsonConvert.DeserializeObject<SigninCard>(JsonConvert.SerializeObject(activity.Attachments.FirstOrDefault().Content));
+                        signInUrl = card.Buttons[0].Value?.ToString();
+
+                        Assert.False(string.IsNullOrEmpty(signInUrl));
+                    });
+
+                    // Execute the SignIn.
+                    await runner.ClientSignInAsync(signInUrl);
+
+                    // Execute the rest of the conversation.
+                    await runner.RunTestAsync(Path.Combine(_transcriptsFolder, "ShouldSignIn2.transcript"));
                 }
                 catch (Exception e)
                 {
