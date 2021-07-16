fd4d193ff73afdfb3929b466c3a3b82dcdbef967
Santiago Grangetto <38112957+santgr11@users.noreply.github.com>
2021-07-16 14:03:48 -0300

    Update index

--- Bots/JavaScript/Consumers/CodeFirst/SimpleHostBot/index.js
+++ Bots/JavaScript/Consumers/CodeFirst/SimpleHostBot/index.js
@@ -3,6 +3,9 @@
 
 // index.js is used to setup and configure your bot
 
+// Import required bot configuration.
+require('dotenv').config();
+
 // Import required packages
 const http = require('http');
 const https = require('https');
@@ -11,44 +14,73 @@ const restify = require('restify');
 
 // Import required bot services.
 // See https://aka.ms/bot-services to learn more about the different parts of a bot.
-const { BotFrameworkAdapter, TurnContext, ActivityTypes, ChannelServiceRoutes, ConversationState, InputHints, MemoryStorage, SkillHandler, SkillHttpClient, MessageFactory, SkillConversationIdFactory } = require('botbuilder');
-const { AuthenticationConfiguration, SimpleCredentialProvider } = require('botframework-connector');
+const {
+  CallerIdConstants,
+  CloudAdapter,
+  TurnContext,
+  ActivityTypes,
+  ChannelServiceRoutes,
+  ConversationState,
+  InputHints,
+  MemoryStorage,
+  SkillHandler,
+  SkillHttpClient,
+  MessageFactory,
+  SkillConversationIdFactory
+} = require('botbuilder');
 
-// Import required bot configuration.
-const ENV_FILE = path.join(__dirname, '.env');
-require('dotenv').config({ path: ENV_FILE });
+const {
+  AuthenticationConfiguration,
+  AuthenticationConstants,
+  BotFrameworkAuthenticationFactory,
+  PasswordServiceClientCredentialFactory,
+  SimpleCredentialProvider,
+  allowedCallersClaimsValidator
+} = require('botframework-connector');
 
 // This bot's main dialog.
 const { HostBot } = require('./bots/hostBot');
 const { SkillsConfiguration } = require('./skillsConfiguration');
-const { allowedSkillsClaimsValidator } = require('./authentication/allowedSkillsClaimsValidator');
 const { SetupDialog } = require('./dialogs/setupDialog');
 
-const maxTotalSockets = (preallocatedSnatPorts, procCount = 1, weight = 0.5, overcommit = 1.1) =>
-  Math.min(
-    Math.floor((preallocatedSnatPorts / procCount) * weight * overcommit),
-    preallocatedSnatPorts
-  );
+// Load skills configuration
+const skillsConfig = new SkillsConfiguration();
 
-// Create adapter.
-// See https://aka.ms/about-bot-adapter to learn more about adapters.
-const adapter = new BotFrameworkAdapter({
-  appId: process.env.MicrosoftAppId,
-  appPassword: process.env.MicrosoftAppPassword,
-  authConfig: new AuthenticationConfiguration([], allowedSkillsClaimsValidator),
-  clientOptions: {
+const allowedCallers = Object.values(skillsConfig.skills).map(skill => skill.appId)
+
+const authConfig = new AuthenticationConfiguration(
+  [],
+  new allowedCallersClaimsValidator(allowedCallers)
+)
+
+const botFrameworkAuthentication = BotFrameworkAuthenticationFactory.create(
+  '',
+  true,
+  AuthenticationConstants.ToChannelFromBotLoginUrl,
+  AuthenticationConstants.ToChannelFromBotOAuthScope,
+  AuthenticationConstants.ToBotFromChannelTokenIssuer,
+  AuthenticationConstants.OAuthUrl,
+  AuthenticationConstants.ToBotFromChannelOpenIdMetadataUrl,
+  AuthenticationConstants.ToBotFromEmulatorOpenIdMetadataUrl,
+  CallerIdConstants.PublicAzureChannel,
+  new PasswordServiceClientCredentialFactory(
+    process.env.MicrosoftAppId || '',
+    process.env.MicrosoftAppPassword || ''
+  ),
+  authConfig,
+  undefined,
+  {
     agentSettings: {
       http: new http.Agent({
         keepAlive: true,
-        maxTotalSockets: maxTotalSockets(1024, 4, 0.3)
-      }),
-      https: new https.Agent({
-        keepAlive: true,
-        maxTotalSockets: maxTotalSockets(1024, 4, 0.7)
       })
     }
   }
-});
+);
+
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about adapters.
+const adapter = new CloudAdapter(botFrameworkAuthentication);
 
 // Catch-all for errors.
 adapter.onTurnError = async (context, error) => {
@@ -127,9 +159,6 @@ const conversationState = new ConversationState(memoryStorage);
 // Create the conversationIdFactory
 const conversationIdFactory = new SkillConversationIdFactory(memoryStorage);
 
-// Load skills configuration
-const skillsConfig = new SkillsConfiguration();
-
 // Create the credential provider;
 const credentialProvider = new SimpleCredentialProvider(process.env.MicrosoftAppId, process.env.MicrosoftAppPassword);
 
@@ -141,7 +170,11 @@ const dialog = new SetupDialog(conversationState, skillsConfig);
 const bot = new HostBot(dialog, conversationState, skillsConfig, skillClient);
 
 // Create HTTP server
-const server = restify.createServer();
+const server = restify.createServer({ maxParamLength: 1000 });
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+
 server.listen(process.env.port || process.env.PORT || 36000, function () {
   console.log(`\n${server.name} listening to ${server.url}`);
   console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
@@ -149,16 +182,15 @@ server.listen(process.env.port || process.env.PORT || 36000, function () {
 });
 
 // Listen for incoming activities and route them to your bot main dialog.
-server.post('/api/messages', (req, res) => {
+server.post('/api/messages', async (req, res) => {
   // Route received a request to adapter for processing
-  adapter.processActivity(req, res, async (turnContext) => {
+  await adapter.process(req, res, async (turnContext) => {
     // route to bot activity handler.
     await bot.run(turnContext);
   });
 });
 
 // Create and initialize the skill classes
-const authConfig = new AuthenticationConfiguration([], allowedSkillsClaimsValidator);
 const handler = new SkillHandler(adapter, bot, conversationIdFactory, credentialProvider, authConfig);
 const skillEndpoint = new ChannelServiceRoutes(handler);
 skillEndpoint.register(server, '/api/skills');
