c8b6f5add253cede8fd0cf07a216be6184f97fe3
sw-joelmut <joel.mut@southworks.com>
2022-01-21 11:30:40 -0300

    Fix project paths and structure

--- Tests/Functional/Skills/FileUpload/FileUploadTests.cs
+++ Tests/Functional/Skills/FileUpload/FileUploadTests.cs
@@ -9,7 +9,7 @@ using Microsoft.Bot.Connector;
 using Microsoft.Bot.Schema;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using SkillFunctionalTests.Common;
+using SkillFunctionalTests.Skills.Common;
 using TranscriptTestRunner;
 using TranscriptTestRunner.XUnit;
 using Xunit;
