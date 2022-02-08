ec75f8fe249af430c1df714c34c6db5bb2e929e4
sw-joelmut <joel.mut@southworks.com>
2022-02-08 17:39:13 -0300

    Add Azure Storage Queues integration tests

---
+++ Tests/Integration/DotNet/Azure/Storage/Queues/AzureQueueBaseFixture.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Storage.Queues;
+using Xunit;
+
+namespace IntegrationTests.Azure.Storage.Queues
+{
+    public class AzureQueueBaseFixture : ConfigurationFixture, IAsyncLifetime
+    {
+        private readonly TimeSpan timeout = TimeSpan.FromSeconds(3);
+
+        public AzureQueueBaseFixture()
+        {
+            ConnectionString = Configuration["Azure:Storage:ConnectionString"];
+        }
+
+        public string ConnectionString { get; private set; }
+
+        protected bool IsRunning { get; private set; }
+
+        public async Task InitializeAsync()
+        {
+            IsRunning = await IsServiceRunning();
+        }
+
+        public Task DisposeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        protected async Task<bool> IsServiceRunning()
+        {
+            try
+            {
+                using var cancellation = new CancellationTokenSource(timeout);
+                await new QueueServiceClient(ConnectionString).GetPropertiesAsync(cancellation.Token);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                const string message = "StorageAccount: Unable to connect to the 'Queues' endpoint.";
+                throw new Exception(message, ex);
+            }
+        }
+    }
+}
