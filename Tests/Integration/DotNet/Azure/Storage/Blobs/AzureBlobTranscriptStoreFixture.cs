a2248aaea6fc0c88c09f60862b062722533c4123
sw-joelmut <joel.mut@southworks.com>
2022-02-16 16:20:49 -0300

    Add Azure Storage Blobs Transcript integration tests

---
+++ Tests/Integration/DotNet/Azure/Storage/Blobs/AzureBlobTranscriptStoreFixture.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Azure;
+using Xunit;
+
+namespace IntegrationTests.Azure.Storage.Blobs
+{
+    [Blobs(containerId: "AzureBlobTranscriptStoreTests")]
+    public class AzureBlobTranscriptStoreFixture : BlobsStorageBaseFixture, IAsyncLifetime
+    {
+        public IDictionary<StorageCase, ITranscriptStore> Storages { get; private set; }
+
+        public new async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+
+            Storages = new Dictionary<StorageCase, ITranscriptStore>
+            {
+                { StorageCase.Default, new AzureBlobTranscriptStore(ConnectionString, ContainerId) },
+            };
+        }
+    }
+}
---
+++ Tests/Integration/DotNet/Azure/Storage/Blobs/AzureBlobTranscriptStoreTests.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace IntegrationTests.Azure.Storage.Blobs
+{
+    [Trait("TestCategory", "Deprecated")]
+    public class AzureBlobTranscriptStoreTests : BlobsTranscriptStoreBaseTests, IClassFixture<AzureBlobTranscriptStoreFixture>
+    {
+        public AzureBlobTranscriptStoreTests(ITestOutputHelper outputHandler, AzureBlobTranscriptStoreFixture blobFixture)
+            : base(outputHandler)
+        {
+            UseStorages(blobFixture.Storages);
+        }
+    }
+}
