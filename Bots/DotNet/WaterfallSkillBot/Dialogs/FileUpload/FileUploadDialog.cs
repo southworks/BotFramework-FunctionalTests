194f16a45f35730d0ab67d0d86e730658cc3c47d
sw-joelmut <joel.mut@southworks.com>
2022-05-09 17:59:30 -0300

    Fix SYSLIB0014 error

--- Bots/DotNet/WaterfallSkillBot/Dialogs/FileUpload/FileUploadDialog.cs
+++ Bots/DotNet/WaterfallSkillBot/Dialogs/FileUpload/FileUploadDialog.cs
@@ -3,6 +3,7 @@
 
 using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
@@ -36,6 +37,7 @@ namespace Microsoft.Bot.Builder.FunctionalTestsBots.WaterfallSkillBot.Dialogs.Fi
         private async Task<DialogTurnResult> HandleAttachmentStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
             var fileText = string.Empty;
+            var client = new HttpClient();
 
             foreach (var file in stepContext.Context.Activity.Attachments)
             {
@@ -43,14 +45,14 @@ namespace Microsoft.Bot.Builder.FunctionalTestsBots.WaterfallSkillBot.Dialogs.Fi
                 var localFileName = Path.Combine(Path.GetTempPath(), file.Name);
                 string fileContent;
 
-#pragma warning disable SYSLIB0014 // Type or member is obsolete
-                using (var webClient = new WebClient())
+                using var stream = await client.GetStreamAsync(remoteFileUrl, cancellationToken);
+                using (var fs = new FileStream(localFileName, FileMode.CreateNew))
                 {
-                    webClient.DownloadFile(remoteFileUrl, localFileName);
-                    using var reader = new StreamReader(localFileName);
-                    fileContent = await reader.ReadToEndAsync();
+                    await stream.CopyToAsync(fs, cancellationToken);
                 }
-#pragma warning restore SYSLIB0014 // Type or member is obsolete
+
+                using var reader = new StreamReader(localFileName);
+                fileContent = await reader.ReadToEndAsync();
 
                 fileText += $"Attachment \"{file.Name}\" has been received.\r\n";
                 fileText += $"File content: {fileContent}\r\n";
